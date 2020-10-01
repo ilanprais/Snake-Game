54efcb64af5af47ee331b87bd00e08101437a465
ilanprais <38957704+ilanprais@users.noreply.github.com>
2020-10-01 13:11:39 +0300

    Adding optional pre text to score

--- objects/Score.py
+++ objects/Score.py
@@ -5,16 +5,20 @@ from util import Color
 
 class Score(GameObject.GameObject):
 
-    def __init__(self, x, y, color):
+    def __init__(self, x, y, color, text):
         super(Score, self).__init__(x, y, color)
         self.__score = 0
+        self.__text = text
 
     def set(self, score):
         self.__score = score
 
+    def get(self):
+        return self.__score
+
     def display(self, surface):
-        font = pygame.font.SysFont('Comic Sans MS', 30)
-        text = font.render(str(self.__score), False, Color.Color.getColor(self.getColor()))
-        surface.blit(text, (self.getX(), self.getY()))
+        font = pygame.font.SysFont('Comic Sans MS', 23)
+        score = font.render(self.__text + str(self.__score), False, Color.Color.getColor(self.getColor()))
+        surface.blit(score, (self.getX(), self.getY()))
 
 
